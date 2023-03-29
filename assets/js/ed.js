9a55922d994a66d11c2db6923cc6f07053d1a568
zoe-translates <116055375+zoe-translates@users.noreply.github.com>
2023-03-29 11:36:46 +0800

    Move the logic of adding listener/observer up one scope

--- assets/js/ed.js
+++ assets/js/ed.js
@@ -1,23 +1,20 @@
 "use strict";
 // 'Back to top' logic
-const intersectionObserver = new IntersectionObserver(function(entries) {
-  const topBtn = document.querySelector('.top-of-site-link');
-  if (topBtn === null) return;
-
-  topBtn.dataset.visible = entries[0].boundingClientRect.y < 0;
-});
-
+const topBtn = document.querySelector('.top-of-site-link');
 const topAnchor = document.getElementById('top-of-site-anchor');
-if (topAnchor !== null) {
+if (topBtn !== null && topAnchor !== null ) {
+  const intersectionObserver = new IntersectionObserver(function(entries) {
+    topBtn.dataset.visible = entries[0].boundingClientRect.y < 0;
+  });
   intersectionObserver.observe(topAnchor);
 }
 
 // set up keyboard cotrol of the menu toggle
 const widget = document.getElementById('sidebar-toggle');
 const box = document.getElementById('sidebar-checkbox');
-if ((widget !== null) && (box !== null)) {
+if (widget !== null && box !== null) {
   widget.addEventListener('keydown', (e) => {
-    if (e.code == 'Enter') {
+    if (e.key == 'Enter') {
       box.checked ^= 1;
     }
   });
